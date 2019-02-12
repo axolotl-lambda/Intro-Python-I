b3177fb97b40a448fa5b5f23b72cec94b1d06ed5
Axolotl <samuelmachat@gmail.com>
2019-02-12 17:46:21 -0500

    fix all pep8 linting issues

--- src/classes.py
+++ src/classes.py
@@ -9,8 +9,9 @@ class LatLon:
         self.lat = lat
         self.lon = lon
 
-# Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
-# constructor. It should inherit from LatLon. Look up the `super` method.
+# Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon`
+# to the constructor. It should inherit from LatLon. Look up the `super`
+# method.
 
 # YOUR CODE HERE
 
